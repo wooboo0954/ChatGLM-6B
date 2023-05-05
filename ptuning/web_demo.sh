2f58fcafdffd7a51231023551224365b692d2297
wooboo0954 <41883021+wooboo0954@users.noreply.github.com>
2023-05-05 21:17:08 +0800

    Update web_demo.sh
    change parameter and run ptuning script

--- ptuning/web_demo.sh
+++ ptuning/web_demo.sh
@@ -1,7 +1,6 @@
-PRE_SEQ_LEN=128
+PRE_SEQ_LEN=256
 
-CUDA_VISIBLE_DEVICES=0 python3 web_demo.py \
+CUDA_VISIBLE_DEVICES=0 python3 /content/ChatGLM-6B/ptuning/web_demo.py \
     --model_name_or_path THUDM/chatglm-6b \
-    --ptuning_checkpoint output/adgen-chatglm-6b-pt-128-2e-2/checkpoint-3000 \
+    --ptuning_checkpoint /content/ChatGLM-6B/output/MyGPT-ChatGLM-256-2e-2 \
     --pre_seq_len $PRE_SEQ_LEN
-
