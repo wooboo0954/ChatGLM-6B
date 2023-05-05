b2f0e7beca07e9cd02471d83c0690842810ae923
wooboo0954 <41883021+wooboo0954@users.noreply.github.com>
2023-05-05 20:24:41 +0800

    Update train_v1.sh

--- ptuning/train_v1.sh
+++ ptuning/train_v1.sh
@@ -1,7 +1,7 @@
 PRE_SEQ_LEN=256
 LR=2e-4
 
-CUDA_VISIBLE_DEVICES=0 python3 main.py \
+CUDA_VISIBLE_DEVICES=0 python3 ChatGLM-6B/main.py \
     --do_train \
     --train_file ptune_data_v1.json \
     --validation_file ptune_data_v1.json \
@@ -11,8 +11,8 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --model_name_or_path THUDM/chatglm-6b \
     --output_dir output/MyGPT-ChatGLM-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
-    --max_source_length 64 \
-    --max_target_length 64 \
+    --max_source_length 256 \
+    --max_target_length 256 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
