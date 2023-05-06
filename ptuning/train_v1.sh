c49b92d63664a9d2edce69615915a7616d41b95a
wooboo0954 <41883021+wooboo0954@users.noreply.github.com>
2023-05-06 14:33:55 +0800

    Update train_v1.sh

--- ptuning/train_v1.sh
+++ ptuning/train_v1.sh
@@ -1,10 +1,10 @@
 PRE_SEQ_LEN=256
-LR=2e-4
+LR=2e-2
 
 CUDA_VISIBLE_DEVICES=0 python3 /content/ChatGLM-6B/ptuning/main.py \
     --do_train \
-    --train_file ptune_data_100.json \
-    --validation_file ptune_data_100.json \
+    --train_file ptune_data_v1.json \
+    --validation_file ptune_data_v1.json \
     --prompt_column instruction \
     --response_column output \
     --overwrite_cache \
@@ -17,10 +17,8 @@ CUDA_VISIBLE_DEVICES=0 python3 /content/ChatGLM-6B/ptuning/main.py \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --predict_with_generate \
-    --max_steps 300 \
+    --max_steps 100 \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN
-
-
