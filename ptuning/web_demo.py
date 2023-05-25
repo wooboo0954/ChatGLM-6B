23fded5f4ebec65d73b0fe407b3b70b6ebf45996
wooboo0954 <41883021+wooboo0954@users.noreply.github.com>
2023-05-25 15:12:38 +0800

    Update web_demo.py
    replace <p> </p> to '' in output

--- ptuning/web_demo.py
+++ ptuning/web_demo.py
@@ -67,6 +67,8 @@ def parse_text(text):
                     line = line.replace("(", "&#40;")
                     line = line.replace(")", "&#41;")
                     line = line.replace("$", "&#36;")
+                    line = line.replace("", "</p>")
+                    line = line.replace("", "<p>")
                 lines[i] = "<br>"+line
     text = "".join(lines)
     return text
